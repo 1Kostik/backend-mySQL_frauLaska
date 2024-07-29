42107dff0383df3fffce96e7ca385c9cc75c36c9
1Kostik <sorochankostya@gmail.com>
2024-07-29 19:52:56 +0300

    up

--- controllers/products/deleteVariations.js
+++ controllers/products/deleteVariations.js
@@ -1,16 +1,16 @@
 const db = require("../../db");
 
-
 const deleteVariations = async (req, res) => {
   const { id } = req.params;
-
   try {
     await deleteVariation(id);
-    res.status(204)
+    res.status(200).json({ message: `Variations with ID ${id} deleted successfully` });
   } catch (error) {
+    console.error("Error deleting variations data:", error);
     res.status(500).send("Error deleting variations data");
   }
 };
+
 
 const deleteVariation = async (id) => {
   const query = "DELETE FROM variations WHERE id = ?";
