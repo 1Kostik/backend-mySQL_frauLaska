aa6728f9538ac288d2e3513b09b2dd40a4bfee60
1Kostik <sorochankostya@gmail.com>
2024-07-29 19:55:53 +0300

    up

--- controllers/products/deleteFeedbacks.js
+++ controllers/products/deleteFeedbacks.js
@@ -1,15 +1,12 @@
 const db = require("../../db");
-const { getAllProducts } = require("./getAllProducts");
+
 
 const deleteFeedbacks = async (req, res) => {
   const { id } = req.params;
 
   try {
     await removeFeedbacks(id);
-
-    const productData = await getAllProducts();
-
-    res.status(200).json(productData);
+    res.status(200).json({ message: `Feedbacks with ID ${id} deleted successfully` });
   } catch (error) {
     res.status(500).send("Error deleting feedbacks data");
   }
