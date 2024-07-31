5839498d1a12207be2d3d9a3aa3322776848d35c
1Kostik <sorochankostya@gmail.com>
2024-07-31 20:02:13 +0300

    update

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const { createOrders, deleteOrder, getOrderByNumber, getAllOrders } = require("../../controllers");
+const {
+  createOrders,
+  deleteOrder,
+  getOrderByNumber,
+  getAllOrders,
+} = require("../../controllers");
 const router = express.Router();
 
 router.get("/orders", getAllOrders);
