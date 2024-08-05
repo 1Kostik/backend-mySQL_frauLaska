ba90acc1c121b9f26d0606be25bc3cd24aa763dc
1Kostik <sorochankostya@gmail.com>
2024-08-05 18:42:55 +0300

    upd createOrders

--- controllers/orders/getOrders.js
+++ controllers/orders/getOrders.js
@@ -2,10 +2,7 @@ const db = require("../../db");
 const fetchOrderWithItemsByNumber = (id) => {
   return new Promise((resolve, reject) => {
     const sql = `
-        SELECT o.*, oi.* 
-        FROM orders o
-        LEFT JOIN order_items oi ON o.id = oi.orders_items_id
-        WHERE o.id = ?`;
+        SELECT * FROM orders WHERE id = ?`;
 
     db.query(sql, [id], (err, data) => {
       if (err) return reject(err);
@@ -13,11 +10,31 @@ const fetchOrderWithItemsByNumber = (id) => {
     });
   });
 };
-
+const fetchOrderItemsById = (orders_items_id) => {
+  return new Promise((resolve, reject) => {
+    const sql = `SELECT * FROM order_items WHERE orders_items_id = ? `;
+    db.query(sql, [orders_items_id], (err, data) => {
+      if (err) return reject(err);
+      resolve(data);
+    });
+  });
+};
 const getOrderByNumber = async (id) => {
   try {
+    let order_items;
+
     const result = await fetchOrderWithItemsByNumber(id);
-    return result;
+
+    if (result && id) {
+      order_items = await fetchOrderItemsById(id);
+    }
+
+    const resultOrders = {
+      ...result[0],
+      order_items,
+    };
+
+    return resultOrders;
   } catch (error) {
     console.error("Error executing query", error);
     throw new Error(
